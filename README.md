037a5d5aea8ae86685e776179a96a62402f17feb
Duke Nukem <garrettduke87@gmail.com>
2021-01-25 19:17:27 -0700

    initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# E-Commerce Backend - ORM Project
+
+## About
+Using Sequelize handle all requests and creates the table. Using Sequelize you can execute SQL commands with plain Javascript. This API allows you to do the basic CRUD operations of the database. 
+
+## Usage
+In order to use/test this API, clone this repo, then in your terminal/command-line run `npm i` after which create the database using the SQL command `CREATE DATABASE ***`. After which exit SQL then run the command `npm run seed` then that will populate the table with test data. From there you can run `npm start` and then you can test it in your favorite API testing program (ie Postman, Insomnia Core, etc)
+
+## Testing
+This has been tested with Insomnia Core for all of the end points. 
+
+## Video of Product in Use
+[Application Video](https://youtu.be/4FLD-jaySxg)
+
+## Technologies used
+* Node.js
+* dotENV
+* Sequelize
+* Express.js
+* mysql2
---
+++ models/Product.js
@@ -0,0 +1,56 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        min: 0.00,
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      value: 10, //Check to see if this is right
+      validate:{ 
+        min: 0,
+        isNumeric: true
+      }
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: "id"
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
